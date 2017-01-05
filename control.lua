c6448c2639d6550f492d4a0a6c14b2dc4d723e3b
Nexela <nexela@gmail.com>
2017-01-05 03:45:48 -0500

    Add addition sanity checking to get_valid_player Changed mod name strings to be consistent with the mods name/title Syntax changes Change from player.character to just player Remove connected and character checks to controllers check Trim the fat from the meat and potatoes Add Interface print_global, and change_modifier_for_all Rename Interfaces Consistent spacing/punction throughout.

--- control.lua
+++ control.lua
@@ -1,6 +1,8 @@
 -- Bounds for expected behaviour: 0.0 > x > -1.0
--- Above 0: becomes FastKey (0 is default modifier when creating player unless changed by mods)
+-- Above 0: becomes FastKey
 -- Below -1.0: inverts movement
+-- 0 is factorio default modifier when creating player.
+-- Other mods can set this either higher or lower
 
 --Default modifier
 local MODIFIER = -0.5
@@ -12,14 +14,14 @@ local function debug(string)
   end
 end
 
---return a valid player from index, string, table
+--Return a valid player from index, string, table
 local function get_valid_player(player)
-  player=player or game.player -- player is blank, see if there is a game.player (console user)
-  if type(player)=="number" or type(player) == "string" then --if an index or name is passed.
-    player=game.players[player]
+  player = player or game.player --If player is blank, see if there is a game.player (console user)
+  if type(player) == "number" or type(player) == "string" then --If an index or name is passed.
+    player = game.players[player]
   end
-  if player and player.valid then --Do we have a valid player now?
-    return player --return the player
+  if player and type(player) == "table" and player.valid then --Do we have a valid player now?
+    return player --Return the player
   else
     return nil
   end
@@ -27,76 +29,88 @@ end
 
 --Create a small text caption on our character when we are slowed.
 local function info_gui(player, slowed)
-  if player.gui.center["slow-key"] then player.gui.center["slow-key"].destroy() end
+  if player.gui.center["slowkey"] then player.gui.center["slowkey"].destroy() end
   if slowed then
-    local gui = player.gui.center.add{type="label", name="slow-key", caption=" (SLOW) ", direction = "vertical"}
+    local gui = player.gui.center.add{type="label", name="slowkey", caption=" (SLOW) ", direction = "vertical"}
     gui.style.minimal_width=32
   end
 end
 
---create the global if not exists, Doing this here will have no noticeable impact on performance.
+--Create the global if not exists, Doing this here will have no noticeable impact on performance.
 local function get_or_create_global(player_index)
-  if not global.player_data then global.player_data = {} end
-  if not global.player_data[player_index] then global.player_data[player_index] = {modifier = MODIFIER, slowed=false} end
-  return global.player_data[player_index] --return the table all in one fell swoop
+  global.player_data = global.player_data or {}
+  global.player_data[player_index] = global.player_data[player_index] or {modifier = MODIFIER}
+  return global.player_data[player_index] --Return the table all in one fell swoop
 end
 
 --The meat and potatoes
 script.on_event("slowkey-slow",
   function(event)
-    local pdata = get_or_create_global(event.player_index) --create local reference to players global data we need
-    local player = game.players[event.player_index] --create local reference to the player
-    --Determine ready to do stuff player
-    if player.connected and player.character and player.controller_type == defines.controllers.character then
+    local pdata = get_or_create_global(event.player_index) --Create local reference to players global mod data
+    local player = game.players[event.player_index] --Create local reference to the player
 
-      local cur_modifier = player.character.character_running_speed_modifier -- Lets shorten this up a bit :)
-      if pdata.slowed then -- We are slowed down, lets speed back up.
-        local change = (pdata.difference + cur_modifier == pdata.last_modifier and pdata.difference) or (cur_modifier + math.abs(pdata.saved_modifier))
-        debug("changing speed modifier to "..cur_modifier + change.. " cur="..cur_modifier.." old="..pdata.last_modifier.." dif="..pdata.difference)
-        player.character.character_running_speed_modifier = cur_modifier + change
+    --Determine if player is ready
+    if player and player.controller_type == defines.controllers.character then
+      local cur_modifier = player.character_running_speed_modifier --Let's shorten this up a bit
+      if pdata.last_modifier then --We are slowed down, let's speed back up.
+        local change = ((pdata.saved_modifier == cur_modifier) and pdata.last_modifier) or cur_modifier
+        debug("SlowKey disable: Change="..change.." cur="..cur_modifier.." last="..pdata.last_modifier)
+        player.character_running_speed_modifier = change
         info_gui(player, false)
-        pdata.slowed=false
-        --free up uneeded variables
-        pdata.difference, pdata.saved_modifier, pdata.last_modifier = nil, nil, nil
-      else -- We are at normal speed, lets slow down
-        pdata.last_modifier = cur_modifier -- Save a copy of the players current modifier in case it gets changed.
-        pdata.saved_modifier = pdata.modifier -- Save a copy of the slow modifier in case we change it.
-        pdata.difference = cur_modifier - pdata.modifier -- get the difference in case players modifier changes some other way.
-        pdata.slowed = true
-        debug("Slowing speed to "..pdata.modifier.. " from="..cur_modifier.." diff="..pdata.difference)
-        player.character.character_running_speed_modifier = pdata.modifier
+        --Free up uneeded variables
+        pdata.saved_modifier, pdata.last_modifier = nil, nil
+      else --We are at normal speed, lets slow down
+        pdata.last_modifier = cur_modifier --Save a copy of the players current modifier in case it gets changed.
+        pdata.saved_modifier = pdata.modifier --Save a copy of the slow modifier in case we change it.
+        player.character_running_speed_modifier = pdata.modifier
         info_gui(player, true)
       end
-
     end
   end
 )
 
 local interface = {}
---Change the modifier amount directly from the command line!
-interface.change_slow_key_modifier = function (value, player)
+--Change the modifier amount directly from the command line
+interface.change_modifier = function (value, player)
   value = value or MODIFIER
   player = get_valid_player(player)
   if player then
     local pdata = get_or_create_global(player.index)
-    local cur_modifier = (player.connected and player.character and player.character.character_running_speed_modifier) or 0
+    local cur_modifier = ((player.controller_type == defines.controllers.character) and player.character_running_speed_modifier) or 0
     if value <= cur_modifier and value > -1 then
       pdata.modifier = value
-      game.player.print("slow-key: New default slow modifier set to "..value)
+      player.print("SlowKey: New default slow modifier set to "..value)
       return value
     else
-      game.player.print("slow-key: Value "..value.." must be greater than current_modifier:"..cur_modifier.." and less than -l")
+      player.print("SlowKey: Value("..value..") must be greater than current_modifier("..cur_modifier..") and less than -l")
     end
   end
 end
---Read the current modifier
-interface.read_slow_key_modifier = function(player)
+
+--Change the modifier for all players
+interface.change_modifier_for_all = function(value)
+  value = value or MODIFIER
+  for _, player in pairs(game.players) do
+    interface.change_modifier(value, player)
+  end
+end
+
+--Return the current modifier
+interface.read_modifier = function(player)
   player = get_valid_player(player)
   if player then
     local pdata = get_or_create_global(player.index)
-    player.print("slow-key: Current modifier is "..pdata.modifier)
+    player.print("SlowKey: Current modifier is "..pdata.modifier)
     return pdata.modifier
   end
 end
---Add Our interface to the game
-remote.add_interface("slow-key", interface)
+
+--Print the global table
+interface.print_global = function()
+  local print_str = "SlowKey: Default Modifier = "..MODIFIER.."\nglobal="..serpent.block(global, {comment=false})
+  game.print(print_str)
+  game.write_file("logs/slow-key/global.txt", print_str, false)
+end
+
+--Add Our interfaces to the game
+remote.add_interface("slowkey", interface)
